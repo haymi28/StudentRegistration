95bc8177a6a4652ae4d3745071ce886dcda99a85
blen Nib <blennib12@gmail.com>
2025-07-03 09:01:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+
 import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import '@js-temporal/polyfill';
 
 const inter = Inter({
   subsets: ['latin'],
